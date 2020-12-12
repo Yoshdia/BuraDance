d8d2f246b3bb68504c1627fd255e0950ee1fbd9f
yutaro ono <ono.yutaro.tec0623@gmail.com>
2020-12-12 16:33:26 +0900

    TutorialScene調整

---
+++ BuraDance/Assets/Yutaro/Scripts/SceneTransition.cs
@@ -0,0 +1,48 @@
+﻿//--------------------------------------------------------------+
+// シーン遷移スクリプト (全シーン共通で使用する)
+// 2020/12/12 YuatroOno
+//--------------------------------------------------------------+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// シーン遷移処理
+    /// 現在のシーン名を取得し、対応する次のシーンをロードする
+    /// </summary>
+    public void ChangeScene()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+
+        if(scene.name == "TitleScene")
+        {
+            SceneManager.LoadScene("TutorialScene");
+        }
+            
+        if(scene.name == "TutorialScene")
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+
+        if(scene.name == "GameScene")
+        {
+            SceneManager.LoadScene("ResultScene");
+        }
+
+    }
+}
