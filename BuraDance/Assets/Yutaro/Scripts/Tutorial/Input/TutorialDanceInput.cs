aa941396ecb357585d1d09a5d3245c983d7dd80b
yutaro ono <ono.yutaro.tec0623@gmail.com>
2020-12-12 11:56:22 +0900

    チュートリアル用のアニメーターとアニメーション追加。踊り用の入力システムを追加。
    入力の問題か、ぶら男・子のタイミングが合わない。TextMeshProが勝手に追加された。

---
+++ BuraDance/Assets/Yutaro/Scripts/Tutorial/Input/TutorialDanceInput.cs
@@ -0,0 +1,59 @@
+﻿//----------------------------------------------------------------------------------------+
+// チュートリアルのダンス入力処理 (左右クリック・左右タッチで対応したポーズ再生)
+// 2020/12/12 YutaroOno
+//----------------------------------------------------------------------------------------+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class TutorialDanceInput : MonoBehaviour
+{
+
+    [SerializeField]
+    TutorialInputManager inputManager;    // inputManagerクラス<ScriptableObejcts>
+    private Animator animator;            // Animatorハンドル
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();    // Animator取得
+        inputManager = GameObject.Find("TutorialInputManager").GetComponent<TutorialInputManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // クリックに応じたアニメーションの再生
+        PlayLeftPose(inputManager.GetLeftTrigger());
+        PlayRightPose(inputManager.GetRightTrigger());
+    }
+
+
+    /// <summary>
+    /// 左クリック時にポーズ(左)アニメーションのトリガーをオン
+    /// </summary>
+    /// <param name="in_Lclick">左クリックしたかどうか</param>
+    void PlayLeftPose(bool in_Lclick)
+    {
+        if(in_Lclick)
+        {
+            animator.SetTrigger("LeftClick");
+        }
+    }
+
+    /// <summary>
+    /// 右クリック時にポーズ(右)アニメーションのトリガーをオン
+    /// </summary>
+    /// <param name="in_Rclick">右クリックしたかどうか</param>
+    void PlayRightPose(bool in_Rclick)
+    {
+        if (in_Rclick)
+        {
+            animator.SetTrigger("RightClick");
+        }
+    }
+}
---
+++ BuraDance/Assets/Yutaro/Scripts/Tutorial/Input/TutorialInputManager.cs
@@ -0,0 +1,63 @@
+﻿//------------------------------------------------------+
+// チュートリアル用入力フラグ管理クラス
+// 2020/12/12 YutaroOno
+//------------------------------------------------------+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialInputManager : MonoBehaviour
+{
+
+    // 左右トリガー (対応する入力が入った場合にオン)
+    private bool LTrigger;
+    private bool RTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 左
+        if(Input.GetMouseButtonDown(0))
+        {
+            LTrigger = true;
+        }
+        else
+        {
+            LTrigger = false;
+        }
+
+        // 右
+        if (Input.GetMouseButtonDown(1))
+        {
+            RTrigger = true;
+        }
+        else
+        {
+            RTrigger = false;
+        }
+    }
+
+    /// <summary>
+    /// 左クリック・タッチ入力ゲッター
+    /// </summary>
+    /// <returns></returns>
+    public bool GetLeftTrigger()
+    {
+        return LTrigger;
+    }
+
+    /// <summary>
+    /// 右クリック・タッチ入力ゲッター
+    /// </summary>
+    /// <returns></returns>
+    public bool GetRightTrigger()
+    {
+        return RTrigger;
+    }
+}
