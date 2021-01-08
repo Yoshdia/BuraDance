ac395e7a8b72cf46d6535db6d22364147733b494
yutaro ono <ono.yutaro.tec0623@gmail.com>
2021-01-08 15:13:53 +0900

    スコアに応じたリザルト画面の変化を実装。簡易的なスコアシステムも用意

---
+++ BuraDance/Assets/Yutaro/Scripts/GameScene/SaveScore.cs
@@ -0,0 +1,38 @@
+﻿//---------------------------------------------------------+
+// スコア保存用クラス
+// 2021 Yutaro Ono
+//---------------------------------------------------------+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveScore : MonoBehaviour
+{
+
+    public static int score;
+    public int score_debug;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        score = 0;
+        score_debug = score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    /// <summary>
+    /// スコア加算関数
+    /// </summary>
+    /// <param name="_addVal">加算するスコアの値</param>
+    public void AddScore(int _addVal)
+    {
+        score += _addVal;
+        score_debug += _addVal;
+    }
+}
---
+++ BuraDance/Assets/Yutaro/Scripts/Result/Result_Decision.cs
@@ -0,0 +1,79 @@
+﻿//--------------------------------------------------------------+
+// 結果判定. スコアに応じて表示する各画像を切り替える
+// 2021 Yutaro Ono.
+//--------------------------------------------------------------+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Result_Decision : MonoBehaviour
+{
+
+    [SerializeField]
+    int[] rankScore;
+
+    // スコアによって変動する画像
+    [SerializeField]
+    GameObject[] m_bg;
+    [SerializeField]
+    GameObject[] m_flowers;
+    [SerializeField]
+    GameObject[] m_flowersS;
+    [SerializeField]
+    GameObject[] m_ranks;
+    [SerializeField]
+    GameObject m_audience;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < m_bg.Length; i++)
+        {
+            m_bg[i].SetActive(false);
+        }
+
+        for (int i = 0; i < m_flowers.Length; i++)
+        {
+            m_flowers[i].SetActive(false);
+        }
+        for (int i = 0; i < m_flowersS.Length; i++)
+        {
+            m_flowersS[i].SetActive(false);
+        }
+        for (int i = 0; i < m_ranks.Length; i++)
+        {
+            m_ranks[i].SetActive(false);
+        }
+
+        m_audience.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(SaveScore.score >= rankScore[0] && SaveScore.score < rankScore[1])
+        {
+            m_bg[1].SetActive(true);
+
+            m_flowers[0].SetActive(true);
+            m_flowers[1].SetActive(true);
+            m_ranks[1].SetActive(true);
+        }
+        else if(SaveScore.score >= rankScore[1])
+        {
+            m_bg[2].SetActive(true);
+
+            m_flowersS[0].SetActive(true);
+            m_flowersS[1].SetActive(true);
+            m_flowersS[2].SetActive(true);
+            m_ranks[2].SetActive(true);
+            m_audience.SetActive(true);
+        }
+        else
+        {
+            m_bg[0].SetActive(true);
+            m_flowers[0].SetActive(true);
+            m_ranks[0].SetActive(true);
+        }
+    }
+}
