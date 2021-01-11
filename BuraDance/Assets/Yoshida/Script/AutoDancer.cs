6edbb3084e4c09b51ef35560a81fdb0c5546d578
Yoshdia <yoshiatsu02@gmail.com>
2021-01-11 19:59:01 +0900

    2段目以降ステップの間隔のリセットが正常じゃなかったため修正

--- BuraDance/Assets/Yoshida/Script/AutoDancer.cs
+++ BuraDance/Assets/Yoshida/Script/AutoDancer.cs
@@ -58,7 +58,7 @@ public class AutoDancer : MonoBehaviour
             }
             if (step != StepDirection.NoStep)
             {
-                stepInterval = (float)_phrase.phraseTime / ((float)_phrase.stepCount );
+                stepInterval = (float)_phrase.phraseTime /*/ ((float)_phrase.stepCount )*/;
             }
         }
     }
